642c22c784f0adc57d4dae0e15e82649ae4cb588
Carlos Eduardo Ferreira Carrinho <carrinhoedu@gmail.com>
2022-01-09 10:10:55 -0300

    test: ensure SignUpController returns 400 if no name is provided

---
+++ src/presentation/controller/signup-controller.spec.ts
@@ -0,0 +1,22 @@
+import { SignUpController } from './signup-controller'
+
+describe('SignUp Controller', () => {
+  it('Should return 400 if no name is provided', async () => {
+    // Given
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'anyemail@mail.com',
+        password: 'password',
+        passwordConfirmation: 'password'
+      }
+    }
+
+    // When
+    const httpResponse = sut.handle(httpRequest)
+
+    // Then
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
