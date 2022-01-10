3235a1dd7616b94dea7d8f4ee138d919644da861
Carlos Eduardo Ferreira Carrinho <carrinhoedu@gmail.com>
2022-01-10 08:47:36 -0300

    feat: ensure SignUpController returns 400 if password confirmation fails

--- src/presentation/controller/signup-controller.ts
+++ src/presentation/controller/signup-controller.ts
@@ -15,8 +15,13 @@ export class SignUpController implements Controller {
       }
     }
 
+    const { email, password, passwordConfirmation } = httpRequest.body
+    if (password !== passwordConfirmation) {
+      return badRequest(new InvalidParamError('passwordConfirmation'))
+    }
+
     try {
-      const isValid = this.emailValidator.isValid(httpRequest.body.email)
+      const isValid = this.emailValidator.isValid(email)
 
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
