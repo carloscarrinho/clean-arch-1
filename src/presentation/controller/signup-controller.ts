deeb2232abef1e6c17b0f0baa388a91ba58f74d1
Carlos Eduardo Ferreira Carrinho <carrinhoedu@gmail.com>
2022-01-10 08:24:12 -0300

    feat: ensure SignUpController returns ServerError if EmailValidator throws an error

--- src/presentation/controller/signup-controller.ts
+++ src/presentation/controller/signup-controller.ts
@@ -1,6 +1,6 @@
 import { InvalidParamError } from '../errors/invalid-param-error'
 import { MissingParamError } from '../errors/missing-param-error'
-import { badRequest } from '../helpers/http-helper'
+import { badRequest, internalServerError } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
 import { HttpRequest, HttpResponse } from '../protocols/http'
@@ -17,17 +17,21 @@ export class SignUpController implements Controller {
       }
     }
 
-    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+    try {
+      const isValid = this.emailValidator.isValid(httpRequest.body.email)
 
-    if (!isValid) {
-      return badRequest(new InvalidParamError('email'))
-    }
-
-    return {
-      statusCode: 200,
-      body: {
-        success: true
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
       }
+
+      return {
+        statusCode: 200,
+        body: {
+          success: true
+        }
+      }
+    } catch (error) {
+      return internalServerError()
     }
   }
 }
