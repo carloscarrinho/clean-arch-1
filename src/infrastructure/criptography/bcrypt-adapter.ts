59f0af9a67bc2ed8b51820affd1810083a368ebb
Carlos Eduardo Ferreira Carrinho <carrinhoedu@gmail.com>
2022-01-12 08:24:31 -0300

    feat: ensure BcryptAdapter returns a hash string on success

--- src/infrastructure/criptography/bcrypt-adapter.ts
+++ src/infrastructure/criptography/bcrypt-adapter.ts
@@ -5,8 +5,6 @@ export class BcryptAdapter implements Encrypter {
   constructor (private readonly salt: number = 12) {}
 
   public async encrypt (value: string): Promise<string> {
-    await bcrypt.hash(value, this.salt)
-
-    return await new Promise(resolve => resolve('hashed_value'))
+    return await bcrypt.hash(value, this.salt)
   }
 }
