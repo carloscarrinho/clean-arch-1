82f58a36dd38ece8382f5de5ef87eddd88438a70
Carlos Eduardo Ferreira Carrinho <carrinhoedu@gmail.com>
2022-01-12 08:52:37 -0300

    chore: add test ignore to grouped protocols files

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/**/index.ts'
+    '!<rootDir>/src/**/index.ts',
+    '!<rootDir>/src/**/*-protocols.ts'
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
