96903dfc2d1c8874770eac9d603f9678bbff11fb
Carlos Eduardo Ferreira Carrinho <carrinhoedu@gmail.com>
2022-01-13 13:08:05 -0300

    test: ensure default content-type as json

---
+++ src/main/config/middlewares/content-type.test.ts
@@ -0,0 +1,29 @@
+import request from 'supertest'
+import app from '../app'
+
+describe('Content-Type', () => {
+  it('Should return default Content-Type as json', async () => {
+    const endpoint = '/test_content_as_json'
+
+    app.get(endpoint, (req, res) => {
+      res.send()
+    })
+
+    await request(app)
+      .get(endpoint)
+      .expect('content-type', /json/)
+  })
+
+  it('Should return Content-Type as xml as forced', async () => {
+    const endpoint = '/test_content_as_xml'
+
+    app.get(endpoint, (req, res) => {
+      res.type('xml')
+      res.send()
+    })
+
+    await request(app)
+      .get(endpoint)
+      .expect('content-type', /xml/)
+  })
+})
