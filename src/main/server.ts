14ab89653abec6759a211e035e0b7401c432b3e0
Carlos Eduardo Ferreira Carrinho <carrinhoedu@gmail.com>
2022-01-13 12:42:18 -0300

    feat: ensure parse response body to json

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,5 @@
-import express from 'express'
+import app from './config/app'
 
 const port = 3333
-const app = express()
 
 app.listen(port, () => console.log(`App running at http://localhost:${port}`))
