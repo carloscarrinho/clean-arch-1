75ac1b4fcada0595e6c1053684d9ed1966cd440b
Carlos Eduardo Ferreira Carrinho <carrinhoedu@gmail.com>
2022-01-17 19:08:02 -0300

    test: ensure LogMongoRepository creates an error log on database

---
+++ src/infrastructure/db/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,35 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { LogMongoRepository } from './log'
+
+const makeSut = (): LogMongoRepository => {
+  return new LogMongoRepository()
+}
+
+describe('Log Mongo Repository ', () => {
+  let errorsCollection: Collection
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    errorsCollection = await MongoHelper.getCollection('errors')
+    await errorsCollection.deleteMany({})
+  })
+
+  it('Should create an error log on database on success', async () => {
+    // Given
+    const sut = makeSut()
+
+    // When
+    await sut.log('any_error')
+
+    // Then
+    const errors = await errorsCollection.countDocuments()
+    expect(errors).toBe(1)
+  })
+})
