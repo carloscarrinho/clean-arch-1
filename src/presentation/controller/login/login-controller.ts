8da911e8137a6ba1cb193788bfac0ba2a02ceb87
Carlos Eduardo Ferreira Carrinho <carrinhoedu@gmail.com>
2022-01-17 20:03:31 -0300

    feat: ensure LoginController returns 400 if no password is provided

--- src/presentation/controller/login/login-controller.ts
+++ src/presentation/controller/login/login-controller.ts
@@ -1,9 +1,17 @@
 import { MissingParamError } from '../../errors'
-import { badRequest } from '../../helpers/http-helper'
+import { badRequest, success } from '../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 
 export class LoginController implements Controller {
   async handle (request: HttpRequest): Promise<HttpResponse> {
-    return badRequest(new MissingParamError('email'))
+    const requiredFields = ['email', 'password']
+
+    for (const field of requiredFields) {
+      if (!request.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
+    }
+
+    return success(null)
   }
 }
