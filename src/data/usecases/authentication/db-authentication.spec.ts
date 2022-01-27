5f78d65cbb03817ec2bb3908fe0dc7a43a7e403c
Carlos Eduardo Ferreira Carrinho <carrinhoedu@gmail.com>
2022-01-27 18:08:29 -0300

    test: ensure DbAuthentication calls HashComparer with correct values

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -1,18 +1,38 @@
 import { AuthenticationModel } from '../../../domain/usecases/authentication'
+import { HashComparer } from '../../protocols/hash-comparer'
 import { LoadByAccountRepository } from '../../protocols/load-by-account-repository'
+import { AccountModel } from '../add-account/db-add-account-protocols'
 import { DbAuthentication } from './db-authentication'
 
-const makeSut = ({ load }: { load?: Function}): DbAuthentication => {
+const makeSut = ({
+  load,
+  compare
+}: {
+  load?: Function
+  compare?: Function
+}): DbAuthentication => {
   const loadAccountByEmailRepository = {
     load: load ?? jest.fn()
   } as unknown as LoadByAccountRepository
 
-  return new DbAuthentication(loadAccountByEmailRepository)
+  const hashComparer = {
+    compare: compare ?? jest.fn()
+  } as unknown as HashComparer
+
+  return new DbAuthentication(loadAccountByEmailRepository, hashComparer)
 }
 
 const makeCredentials = (data?: object): AuthenticationModel => ({
   email: 'any_email@mail.com',
   password: 'any_password',
+  ...data
+})
+
+const makeAccount = (data?: object): AccountModel => ({
+  id: 'any_id',
+  name: 'any_name',
+  email: 'any_email@mail.com',
+  password: 'hashed_password',
   ...data
 })
 
@@ -50,9 +70,29 @@ describe('DbAuthentication UseCase', () => {
     const credentials = makeCredentials()
 
     // When
-    const account = await sut.auth(credentials)
+    const result = await sut.auth(credentials)
 
     // Then
-    expect(account).toBe(null)
+    expect(result).toBe(null)
+  })
+
+  it('Should call HashComparer with correct values', async () => {
+    // Given
+    const account = makeAccount()
+    const dependencies = {
+      load: jest.fn().mockResolvedValueOnce(account),
+      compare: jest.fn()
+    }
+    const sut = makeSut(dependencies)
+    const credentials = makeCredentials()
+
+    // When
+    await sut.auth(credentials)
+
+    // Then
+    expect(dependencies.compare).toHaveBeenCalledWith(
+      credentials.password,
+      account.password
+    )
   })
 })
