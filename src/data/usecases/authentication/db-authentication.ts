6734dd910276f6f98ffca5a36dc8726414fbfb67
Carlos Eduardo Ferreira Carrinho <carrinhoedu@gmail.com>
2022-01-27 18:29:41 -0300

    feat: ensure DbAuthentication calls TokenGenerator with received id

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -3,19 +3,28 @@ import {
   AuthenticationModel
 } from '../../../domain/usecases/authentication'
 import { HashComparer } from '../../protocols/cryptography/hash-comparer'
+import { TokenGenerator } from '../../protocols/cryptography/token-generator'
 import { LoadByAccountRepository } from '../../protocols/db/load-by-account-repository'
 
 export class DbAuthentication implements Authentication {
   constructor (
     private readonly repository: LoadByAccountRepository,
-    private readonly hashComparer: HashComparer
+    private readonly hashComparer: HashComparer,
+    private readonly tokenGenerator: TokenGenerator
   ) {}
 
   async auth (credentials: AuthenticationModel): Promise<string> {
     const account = await this.repository.load(credentials.email)
     if (!account) return null
 
-    await this.hashComparer.compare(credentials.password, account.password)
+    const isValid = await this.hashComparer.compare(
+      credentials.password,
+      account.password
+    )
+
+    if (!isValid) return null
+
+    await this.tokenGenerator.generate(account.id)
 
     return null
   }
