a71f07b097f29b4466205c69cd7fad3f794c5a73
Carlos Eduardo Ferreira Carrinho <carrinhoedu@gmail.com>
2022-01-28 12:49:05 -0300

    feat: ensure AccountMongoRepository returns an account on loadByEmail success

--- src/infrastructure/db/mongodb/account-repository/account.ts
+++ src/infrastructure/db/mongodb/account-repository/account.ts
@@ -1,9 +1,10 @@
 import { AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
+import { LoadByAccountRepository } from '../../../../data/protocols/db/load-by-account-repository'
 import { AccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadByAccountRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountsCollection = await MongoHelper.getCollection('accounts')
     const { insertedId } = await accountsCollection.insertOne(accountData)
@@ -12,6 +13,18 @@ export class AccountMongoRepository implements AddAccountRepository {
       name: accountData.name,
       email: accountData.email,
       password: accountData.password
+    }
+  }
+
+  async loadByEmail (email: string): Promise<AccountModel> {
+    const accountsCollection = await MongoHelper.getCollection('accounts')
+    const account = await accountsCollection.findOne({ email })
+
+    return {
+      id: account._id.toString(),
+      name: account.name,
+      email: account.email,
+      password: account.password
     }
   }
 }
