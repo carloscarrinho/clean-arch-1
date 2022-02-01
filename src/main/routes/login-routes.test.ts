7acdd9f3905a5b162b424654e65958b8a3419dac
Carlos Eduardo Ferreira Carrinho <carrinhoedu@gmail.com>
2022-02-01 18:07:47 -0300

    test: add login route

---
+++ src/main/routes/login-routes.test.ts
@@ -0,0 +1,56 @@
+import { hash } from 'bcrypt'
+import { Collection } from 'mongodb'
+import request from 'supertest'
+import { MongoHelper } from '../../infrastructure/db/mongodb/helpers/mongo-helper'
+import app from '../config/app'
+
+let accountsCollection: Collection
+
+describe('Login Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    accountsCollection = await MongoHelper.getCollection('accounts')
+    await accountsCollection.deleteMany({})
+  })
+
+  describe('POST /signup', () => {
+    it('Should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Carlos',
+          email: 'carlos@mail.com',
+          password: '12345',
+          passwordConfirmation: '12345'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    it('Should return 200 on login', async () => {
+      const password = await hash('123456', 12)
+
+      await accountsCollection.insertOne({
+        name: 'Carlos',
+        email: 'carlos@mail.com',
+        password
+      })
+
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'carlos@mail.com',
+          password: '123456'
+        })
+        .expect(200)
+    })
+  })
+})
